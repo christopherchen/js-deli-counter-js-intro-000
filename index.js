6b7149f799cb4c5ea4a79b6e569fec1b7e2a5085
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-09 18:22:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(line) {
+  if (line.length > 0) {
+    var serve = line.shift();
+    return `Currently serving ${serve}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(line) {
+  for (var i = 0; i < line.length; i++) {
+    line.push(`[i]. ${line[i]}`);
+  }
+    
+  if (line.length === 0) {
+    return "The line is currently empty.";   
+  } else {
+    return `The line is currently: ${line}`;
+  }
+}
