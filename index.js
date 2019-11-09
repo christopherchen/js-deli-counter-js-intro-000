9fda401fad6b2ee4a0fc0e3eadc6414856bfd0ca
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-09 18:25:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
-  for (var i = 0; i < line.length; i++) {
-    line.push(`[i]. ${line[i]}`);
-  }
-    
+  
   if (line.length === 0) {
     return "The line is currently empty.";   
   } else {
