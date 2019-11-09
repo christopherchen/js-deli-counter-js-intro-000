637ee0f74a2808d1852357db30e9ec11ae830a1c
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-09 18:28:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(line) {
 function currentLine(line) {
   var katzDeliLine = [];
   for (var i = 0; i < line.length; i++) {
-    line.push(`[i]. ${line[i]}`);
+    line.push(`[i]. ${katzDeliLine[i]}`);
   }
     
   if (line.length === 0) {
